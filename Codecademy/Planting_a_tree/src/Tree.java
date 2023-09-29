9f625168cf58810eee239ee073c053449d3c9c60
phuongtrieu97coder <phuongcoderinfinity@gmail.com>
2023-09-28 22:31:16 -0700

    cmit_09_28_2023_10:31:PM

---
+++ Codecademy/Planting_a_tree/src/Tree.java
@@ -0,0 +1,21 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+    /*
+    We’re going to introduce ourselves and print a tree to the screen.
+    */
+    System.out.println("Hey there, I'm HP and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");   
+    System.out.println("\n");
+    System.out.println("* ** *");
+    System.out.println("******");
+    System.out.println(" **** ");
+    System.out.println("  **  ");
+    System.out.println("  **  ");   
+  }
+}
